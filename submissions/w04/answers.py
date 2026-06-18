aa4e1bb6403d34fe645fa93309830ffe1e775f7a
nadineoct <nadineoctavia2110@gmail.com>
2026-06-18 21:55:03 +0700

    Fix w04 answers

--- submissions/w04/answers.py
+++ submissions/w04/answers.py
@@ -16,7 +16,7 @@ def q01() -> bool:
 
 def q02() -> bool:
     """[T/F] Probabilitas kondisional P(A|B) selalu sama dengan P(B|A)."""
-    return True
+    return False
 
 def q03() -> bool:
     """[T/F] Teorema Bayes memungkinkan kita untuk membalik kondisi probabilitas dari
